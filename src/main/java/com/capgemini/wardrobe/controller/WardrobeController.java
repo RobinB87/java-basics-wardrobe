e5cf2b7ebb1ebaba676bb950ec52930437dd511b
RobinB87 <38104646+RobinB87@users.noreply.github.com>
2018-04-24 19:05:52 +0200

    try

--- src/main/java/com/capgemini/wardrobe/controller/WardrobeController.java
+++ src/main/java/com/capgemini/wardrobe/controller/WardrobeController.java
@@ -8,29 +8,43 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 
-//HTTP requests are handled by a controller.
-//controller class acts on both the model and the view (view = visualization of data (print))
+/**
+ * HTTP requests are handled by a controller.
+ * These components are easily identified by the @RestController annotation
+ * Class controller handles GET requests for /wardrobe by returning a new instance of the Wardrobe class:
+ *
+ * Controller class acts on both the model and the view (view = visualization of data (print))
+ */
+
+
 
 @RestController
 @RequestMapping("/api/wardrobe")
 public class WardrobeController {
 
-    private static ArrayList<Wardrobe> wardrobeList = new ArrayList<>();
-
     /**
-     * wardrobe functionalities
+     * Create wardrobe list
+     * Create wardrobe instance
+     * Create getWardrobe function
      */
 
+    private static ArrayList<Wardrobe> wardrobeList = new ArrayList<>();
+
     @GetMapping("/{id}/{name}")
-    public void addList(@PathVariable int id, @PathVariable String name){
+    public void addWardrobe(@PathVariable int id, @PathVariable String name){
         Wardrobe wardrobe = new Wardrobe(id, name);
         wardrobeList.add(wardrobe);
     }
 
-    @GetMapping("/allnames")
+    @GetMapping("/")
     public ArrayList<Wardrobe> getAllWardrobes(){
         return wardrobeList;
     }
+
+
+    /**
+     * Wardrobe functionalities
+     */
 
     public void open(){
     }
