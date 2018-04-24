43956f3f460311f4d8869f405bd7fc8788c4aa1f
RobinB87 <38104646+RobinB87@users.noreply.github.com>
2018-04-24 17:39:24 +0200

    Buttons added

--- src/main/java/com/capgemini/wardrobe/controller/WardrobeController.java
+++ src/main/java/com/capgemini/wardrobe/controller/WardrobeController.java
@@ -1,12 +1,50 @@
 package com.capgemini.wardrobe.controller;
 
+import com.capgemini.wardrobe.model.Wardrobe;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-public class WardrobeController {
+import java.util.ArrayList;
 
-    //controller class acts on both the model and the view (view = visualization of data (print))
+
+//controller class acts on both the model and the view (view = visualization of data (print))
 
 @RestController
-    @RequestMapping("/api/")
+@RequestMapping("/api/wardrobe")
+public class WardrobeController {
+
+    private static ArrayList<Wardrobe> wardrobeList = new ArrayList<>();
+
+    /**
+     * wardrobe functionalities
+     */
+
+    @GetMapping("/{name}")
+    public void addList(@PathVariable String name){
+        Wardrobe wardrobe = new Wardrobe(name);
+        wardrobeList.add(wardrobe);
+    }
+
+    @GetMapping("/allnames")
+    public ArrayList<Wardrobe> getAllWardrobes(){
+        return wardrobeList;
+    }
+
+    public void open(){
+    }
+
+    public void close(){
+    }
+
+    public void getIn(){
+    }
+
+    public void getOut(){
+    }
+
+    public int kick(int force){
+        return force;
+    }
 }
--- src/main/java/com/capgemini/wardrobe/model/Wardrobe.java
+++ src/main/java/com/capgemini/wardrobe/model/Wardrobe.java
@@ -3,35 +3,47 @@ package com.capgemini.wardrobe.model;
 public class Wardrobe {
 
     private String name = "";
+    private boolean isOpen;
+    private boolean isInside;
+    private boolean isBroken;
 
+    //parameterloze constructor
     public Wardrobe(){}
 
+    //
     public Wardrobe(String name){
         this.name = name;
     }
 
-    public String getName(){
+    public String getName() {
         return name;
     }
 
-    public void setName(){
+    public void setName(String name) {
         this.name = name;
     }
 
-
-    public void open(){
+    public boolean isOpen() {
+        return isOpen;
     }
 
-    public void close(){
+    public void setOpen(boolean open) {
+        isOpen = open;
     }
 
-    public void getIn(){
+    public boolean isInside() {
+        return isInside;
     }
 
-    public void getOut(){
+    public void setInside(boolean inside) {
+        isInside = inside;
     }
 
-    public int kick(int force){
-        return force;
+    public boolean isBroken() {
+        return isBroken;
+    }
+
+    public void setBroken(boolean broken) {
+        isBroken = broken;
     }
 }
