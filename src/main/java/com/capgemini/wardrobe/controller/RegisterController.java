c8a7dcdc4464e59e03d6d2250fa402409153e23e
RobinB87 <38104646+RobinB87@users.noreply.github.com>
2018-04-24 12:23:16 +0200

    no message

--- src/main/java/com/capgemini/wardrobe/controller/RegisterController.java
+++ src/main/java/com/capgemini/wardrobe/controller/RegisterController.java
@@ -1,4 +1,12 @@
 package com.capgemini.wardrobe.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 public class RegisterController {
+
+    //controller class acts on both the model and the view (view = visualization of data (print))
+
+@RestController
+    @RequestMapping("/api/")
 }
--- src/main/java/com/capgemini/wardrobe/model/Wardrobe.java
+++ src/main/java/com/capgemini/wardrobe/model/Wardrobe.java
@@ -1,4 +1,37 @@
 package com.capgemini.wardrobe.model;
 
 public class Wardrobe {
+
+    private String name = "";
+
+    public Wardrobe(){}
+
+    public Wardrobe(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(){
+        this.name = name;
+    }
+
+
+    public void open(){
+    }
+
+    public void close(){
+    }
+
+    public void getIn(){
+    }
+
+    public void getOut(){
+    }
+
+    public int kick(int force){
+        return force;
+    }
 }
