be41cba841eb4f0bf9dc295a3f47ac058942e766
RobinB87 <38104646+RobinB87@users.noreply.github.com>
2018-04-24 18:09:58 +0200

    Changed buttons, added save functionality

---
+++ src/main/resources/static/registry.js
@@ -0,0 +1,33 @@
+function getWardrobes() {
+    $.get("/api/wardrobe/", function (data) {
+
+        if (data.length <= 0){
+            return
+        }
+
+        var html = "";
+
+        for (var i = 0; i < data.length; i++) {
+
+            html += "<p>" + data[i].name + " - " + "</p>";
+        }
+        $("#wardrobes").html(html);
+    });
+}
+
+function saveWardrobe(e) {
+    e.preventDefault();
+
+    var name = $("#name").val();
+
+    console.log("Name of the wardrobe: " + name);
+
+    $.get("/api/wardrobe/" + name + "/", function() {
+        getWardrobes();
+    });  //deze get verwijst naar de @GetMapping
+
+    $("#name").val("");
+}
+
+$("#saveForm").submit(saveWardrobe);
+getWardrobes();
