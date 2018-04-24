00f876245ea9dca31547be91ed05f30d5a508dbc
RobinB87 <38104646+RobinB87@users.noreply.github.com>
2018-04-24 19:47:05 +0200

    added id and made this available in the log

--- src/main/java/com/capgemini/wardrobe/controller/WardrobeController.java
+++ src/main/java/com/capgemini/wardrobe/controller/WardrobeController.java
@@ -17,7 +17,6 @@ import java.util.ArrayList;
  */
 
 
-
 @RestController
 @RequestMapping("/api/wardrobe")
 public class WardrobeController {
--- src/main/resources/static/registry.js
+++ src/main/resources/static/registry.js
@@ -1,5 +1,5 @@
 function getWardrobes() {
-    $.get("/api/wardrobe", function (data) {
+    $.get("/api/wardrobe/", function (data) {
 
         if (data.length <= 0){
             return
@@ -9,7 +9,7 @@ function getWardrobes() {
 
         for (var i = 0; i < data.length; i++) {
 
-            html += "<p>" + data[i].name + " - " + "</p>";
+            html += "<p>" + data[i].name + " - " + data[i].id + "</p>";
         }
         $("#wardrobes").html(html);
     });
@@ -19,12 +19,14 @@ function saveWardrobe(e) {
     e.preventDefault();
 
     var name = $("#name").val();
+    var id = $("#id").val();
 
-    console.log("Name of the wardrobe: " + name);
+    console.log("Name of the wardrobe: " + name + ", id: " + id);
 
-    $.get("/api/wardrobe" + name + "/", function() {
+    $.get("/api/wardrobe/" + name + "/" + id, function() {
         getWardrobes();
-    });  //deze get verwijst naar de @GetMapping
+    });
+    //deze get verwijst naar de @GetMapping
 
     $("#name").val("");
 }
